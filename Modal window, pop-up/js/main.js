3e599a66fbb0e8d33272e6400be881632b435e01
Iryna <132995571+IrynaKhonina@users.noreply.github.com>
2024-08-12 19:09:56 +0300

    Add files via upload

---
+++ Modal window, pop-up/js/main.js
@@ -0,0 +1,39 @@
+const dialog = document.querySelector(".dialog")
+const openModal = document.querySelector(".open-modal")
+const closeModal = dialog.querySelector(".close-modal")
+
+
+function closeOnOverlayClick({ currentTarget, target }) {
+    const dialog = currentTarget
+    const isOnOverlayClick = target === dialog
+    if (OnOverlayClick) {
+        close
+    }
+}
+
+function openModalAndBlockScroll() {
+    dialog.showModal()
+    document.body.classList.add("scroll-block")
+}
+
+function returnScroll() {
+    document.body.classList.remove("scroll-block")
+}
+
+function close() {
+    dialog.close()
+    returnScroll()
+}
+
+
+openModal.addEventListener("click", openModalAndBlockScroll)
+closeModal.addEventListener("click", () => {
+    close()
+})
+
+dialog.addEventListener("click", closeOnOverlayClick)
+dialog.addEventListener("cancel", () => {
+    returnScroll()
+}
+
+);
